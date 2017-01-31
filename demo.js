b04023ce2ddd1f1a527275a06274318639463181
Martin Probst <martin.probst@me.com>
2017-01-31 14:47:44 +0100

    update

--- demo.js
+++ demo.js
@@ -21,8 +21,7 @@ var iid = setInterval(function() {
       console.warn('' + err);
     } else {
       var elapsed = end - start;
-      console.log("adc: %s, volt: %s%%, time: %dms",
-                adc[0].toFixed(1), volt[0].toFixed(1), end - start);
+      //console.log("adc: %s, volt: %s%%, time: %dms", adc[0].toFixed(1), volt[0].toFixed(1), end - start);
     }
   });
 }, 1000); // 2500
