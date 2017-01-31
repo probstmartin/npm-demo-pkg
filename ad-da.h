90d4bc86bae1dfcba9394533f324d6e62751795d
Martin Probst <martin.probst@me.com>
2017-01-31 13:42:54 +0100

    update

---
+++ ad-da.h
@@ -0,0 +1,35 @@
+#ifndef _DHT_H
+#define _DHT_H
+
+
+
+// void  bsp_DelayUS(uint64_t micros);
+// void ADS1256_StartScan(uint8_t _ucScanMode);
+// static void ADS1256_Send8Bit(uint8_t _data);
+// void ADS1256_CfgADC(ADS1256_GAIN_E _gain, ADS1256_DRATE_E _drate);
+// static void ADS1256_DelayDATA(void);
+// static uint8_t ADS1256_Recive8Bit(void);
+// static void ADS1256_WriteReg(uint8_t _RegID, uint8_t _RegValue);
+// static uint8_t ADS1256_ReadReg(uint8_t _RegID);
+// static void ADS1256_WriteCmd(uint8_t _cmd);
+// uint8_t ADS1256_ReadChipID(void);
+// static void ADS1256_SetChannal(uint8_t _ch);
+// static void ADS1256_SetDiffChannal(uint8_t _ch);
+// static void ADS1256_WaitDRDY(void);
+// static int32_t ADS1256_ReadData(void);
+
+// int32_t ADS1256_GetAdc(uint8_t _ch);
+// void ADS1256_ISR(void);
+// uint8_t ADS1256_Scan(void);
+
+
+/***************************************************/
+void Write_DAC8552(uint8_t channel, uint16_t Data);
+uint16_t Voltage_Convert(float Vref, float voltage);
+
+
+int initialize();
+unsigned long long getTime();
+long readADC(int32_t adc[8], int32_t volt[8]);
+
+#endif
--- npm_demo.cpp
+++ npm_demo.cpp
@@ -1,12 +1,18 @@
 #include <node.h>
 #include <nan.h>
 #include <unistd.h>
+#include "ad-da.h"
 
 using namespace v8;
 
+extern int initialized;
+
+int _channel = 0; // default ADC channel
+int _max_retries = 3;
+
 class ReadWorker : public Nan::AsyncWorker {
   public:
-    ReadWorker(Nan::Callback *callback): Nan::AsyncWorker(callback) {
+    ReadWorker(Nan::Callback *callback, int channel): Nan::AsyncWorker(callback), channel(channel) {
 
     }
 
@@ -19,6 +25,7 @@ class ReadWorker : public Nan::AsyncWorker {
     }
 
   private:
+    int channel;
 
     void Init() {
     }
@@ -28,25 +35,81 @@ class ReadWorker : public Nan::AsyncWorker {
 };
 
 void ReadAsync(const Nan::FunctionCallbackInfo<Value>& args) {
-  Nan::Callback *callback = new Nan::Callback(args[0].As<Function>());
-  Nan::AsyncQueueWorker(new ReadWorker(callback));
+  int channel = Nan::To<int>(args[0]).FromJust();
+  Nan::Callback *callback = new Nan::Callback(args[1].As<Function>());
+
+  Nan::AsyncQueueWorker(new ReadWorker(callback, channel));
 }
 
-void ReadSync(const Nan::FunctionCallbackInfo<Value>& args) { 
+void ReadSync(const Nan::FunctionCallbackInfo<Value>& args) {
+  int channel;
+
+  if (args.Length() == 1) {
+    
+    channel = args[0]->Uint32Value();
+
+    // initialization (on demand)
+    if (!initialized) {
+      initialized = initialize() == 0;
+      if (!initialized) {
+        Nan::ThrowTypeError("failed to initialize");
+        return;
+      }
+    }
+  } else {
+    channel = _channel;
+  }
+
+  int32_t adc[8];
+  int32_t volt[8];
+
+  int retry = _max_retries;
+  int result = 0;
+  while (true) {
+    result = readADC(adc, volt);
+    if (result == 0 || --retry < 0) break;
+    usleep(450000);
+  }
+
+  Local<Object> readout = Nan::New<Object>();
+  readout->Set(Nan::New("humidity").ToLocalChecked(), Nan::New<Number>(humidity));
+  readout->Set(Nan::New("temperature").ToLocalChecked(), Nan::New<Number>(temperature));
+  readout->Set(Nan::New("isValid").ToLocalChecked(), Nan::New<Boolean>(result == 0));
+  readout->Set(Nan::New("errors").ToLocalChecked(), Nan::New<Number>(_max_retries - retry));
+
+  args.GetReturnValue().Set(readout);
 }
 
 void Read(const Nan::FunctionCallbackInfo<Value>& args) {
   printf("Enter Read function... \n");
+  int params = args.Length();
+  switch(params) {
+    case 1: // AD channel given
+      ReadSync(args);
+      break;
+    default:
+      Nan::ThrowTypeError("invalid number of arguments");
+  }
+}
+
+void SetMaxRetries(const Nan::FunctionCallbackInfo<Value>& args) {
+    if (args.Length() != 1) {
+      Nan::ThrowTypeError("Wrong number of arguments");
+      return;
+    }
+    _max_retries = args[0]->Uint32Value();
 }
 
 void Initialize(const Nan::FunctionCallbackInfo<Value>& args) {
   printf("Enter Initialize function... \n");
+
 }
 
 void Init(Handle<Object> exports) {
   printf("Enter Init function... \n");
 	Nan::SetMethod(exports, "read", Read);
 	Nan::SetMethod(exports, "initialize", Initialize);
+  Nan::SetMethod(exports, "setMaxRetries", SetMaxRetries);
 }
 
 NODE_MODULE(npm_demo, Init);
